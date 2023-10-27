ccdfafca2d95cc3ee196a5e057dc1cecd299ab01
Cristian Aguada <>
2023-10-27 18:32:41 -0300

    feat(structure): Updated first structure

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CartWidget from './CartWidget'
+
+const NavBar = () => {
+  return (
+    <div>
+        <h3>Brand</h3>
+        <h3>Desplegable</h3>
+        <CartWidget />
+    </div>
+  )
+}
+
+export default NavBar
