e093c6b527995cc8185ecad87628658144cc3961
Cristian <aguadacristian@gmail.com>
2023-11-18 08:52:22 -0300

    refactor(CartWidget): Solved compatibilities

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import Badge from "@material-ui/core/Badge";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Badge from '@mui/material/Badge';
 import { IconButton } from '@chakra-ui/react'
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart"
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
-const CartWidget = () => {
+const theme = createTheme();
+
+const CartWidget = ({itemCount}) => {
   return (
-      <Badge color="secondary" badgeContent={4} overlap="rectangular">
+    <ThemeProvider theme={theme}>
+      <Badge color="secondary" badgeContent={itemCount} overlap="rectangular">
         <IconButton aria-label='Search database' icon={<ShoppingCartIcon />} />
       </Badge>
+    </ThemeProvider>
   )
 }
 
